2dffe9808722bc3f9817ae893273ed9e4df74ee4
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-01-16 15:49:04 +0000

    🎮 Made the maze into an object

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,43 @@
 var canvas = document.querySelector("#maze");
 var ctx = canvas.getContext("2d");
-var maze = [
-	[1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
-	[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
-	[1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
-	[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-	[1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-	[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
-	[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
-	[1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-	[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-	[1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1],
-];
+// var maze = [
+// 	[1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+// 	[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+// 	[1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+// 	[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+// 	[1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+// 	[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+// 	[1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+// 	[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+// 	[1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+// 	[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+// 	[1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1],
+// ];
+
+var maze1 = {
+	map: [
+		[1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+		[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+		[1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+		[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+		[1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+		[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+		[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+		[1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+		[1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+		[1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1],
+	],
+	exitX: 5,
+	exitY: 10,
+};
+
+function drawExit(maze) {
+	ctx.fillStyle = "purple";
+	ctx.fillRect(maze.exitX * cellSize, maze.exitY * cellSize, cellSize, cellSize);
+}
+
+var maze = maze1.map;
 
 var cellSize = 45;
 
@@ -42,6 +67,7 @@ var player = {
 // Draw the player character
 ctx.fillStyle = player.color;
 ctx.fillRect(player.x * cellSize + 10, player.y * cellSize + 10, player.size, player.size);
+drawExit(maze1);
 
 // Move the player character
 document.onkeydown = function (e) {
@@ -54,6 +80,9 @@ document.onkeydown = function (e) {
 	} else if (e.key === "ArrowDown" && maze[player.y + 1][player.x] !== 1) {
 		player.y++;
 	}
+
+	// if (player.y === )
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	// Redraw the maze
@@ -70,6 +99,7 @@ document.onkeydown = function (e) {
 			}
 		}
 	}
+	drawExit(maze1);
 
 	// Redraw the player character
 	ctx.fillStyle = player.color;
